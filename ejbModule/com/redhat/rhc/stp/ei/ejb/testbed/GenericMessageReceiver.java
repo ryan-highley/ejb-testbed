b35e53141ef7c6c2b6c41f4559bb91a5adf14ace
Ryan Highley <rhighley@redhat.com>
2021-02-12 14:11:06 -0600

    Initial import

---
+++ ejbModule/com/redhat/rhc/stp/ei/ejb/testbed/GenericMessageReceiver.java
@@ -0,0 +1,32 @@
+package com.redhat.rhc.stp.ei.ejb.testbed;
+
+import javax.ejb.ActivationConfigProperty;
+import javax.ejb.MessageDriven;
+import javax.jms.Message;
+import javax.jms.MessageListener;
+
+/**
+ * Message-Driven Bean implementation class for: GenericMessageReceiver
+ */
+@MessageDriven(
+		activationConfig = { @ActivationConfigProperty(
+				propertyName = "destinationType", propertyValue = "javax.jms.Queue")
+		})
+public class GenericMessageReceiver implements MessageListener {
+
+    /**
+     * Default constructor. 
+     */
+    public GenericMessageReceiver() {
+        // TODO Auto-generated constructor stub
+    }
+	
+	/**
+     * @see MessageListener#onMessage(Message)
+     */
+    public void onMessage(Message message) {
+        // TODO Auto-generated method stub
+        
+    }
+
+}
